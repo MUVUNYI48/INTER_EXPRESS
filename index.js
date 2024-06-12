4c251708b4f036315ea5dd1eeb5a1b2b2c457f5e
unknown <patrickmuvunyi@gmail.com>
2024-06-12 11:36:27 +0200

    mysql is now connected

--- index.js
+++ index.js
@@ -1,32 +1,42 @@
-const express=require('express' );
-const app=express();
+const express = require('express');
+const mysql = require('mysql2');
+const app = express();
+const port = 3000;
 
-app.use(express.static('public'));
+// Create MySQL connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '12345',
+    database: 'sakila'
+});
 
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL:', err.stack);
+        return;
+    }
+    console.log('Connected to MySQL as id ' + connection.threadId);
+});
+
+// Basic route
 app.get('/', (req, res) => {
-    res.send('Hey Developers!');
-  });
-  
-app.listen(3000,(req,res)=>{
-    console.log('server is ready');
-})
+    res.send('Hello World!');
+});
 
+// Route to get all users
+app.get('/users', (req, res) => {
+    connection.query('SELECT * FROM actor limit 5', (error, results) => {
+        if (error) {
+            console.error('Error executing query:', error.stack);
+            res.status(500).send('Database query error');
+            return;
+        }
+        res.json(results);
+    });
+});
 
-
-// const cb0 = function (req, res, next) {
-//     console.log('CB0')
-//     next()
-//   }
-  
-//   const cb1 = function (req, res, next) {
-//     console.log('CB1')
-//     next()
-//   }
-  
-//   app.get('/example/d', [cb0, cb1], (req, res, next) => {
-//     console.log('the response will be sent by the next function ...')
-//     next()
-//   }, (req, res) => {
-//     res.send('Hello from D!')
-//   })
-  
+// Start server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
